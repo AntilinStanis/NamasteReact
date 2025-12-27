1db3f254efd000d2694d01591391ffae06e49289
AntilinStanis <antilinstanish@gmail.com>
2025-12-27 20:22:33 +0530

    removed coverage folder from the repo

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,12 +1,30 @@
-import { render ,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom"
 
-test("Should load Contact Us component",()=>{
+test("Should load Contact Us component", () => {
    render(<Contact />);
 
    const heading = screen.getByRole("heading");
-   console.log(heading);
+   // console.log(heading);
    //Assertion
    expect(heading).toBeInTheDocument();
+});
+
+test("Should load the button with submit text", () => {
+   render(<Contact />);
+
+   const buttonText = screen.getByText("Submit");
+   // console.log(buttonText);
+   //Assertion
+   expect(buttonText).toBeInTheDocument();
+});
+
+test('Should load 3 input boxes', () => {
+   render(<Contact />);
+
+   const inputBoxes = screen.getAllByRole("textbox");
+   console.log(inputBoxes);
+   expect(inputBoxes.length).toBe(3);
+
 });
